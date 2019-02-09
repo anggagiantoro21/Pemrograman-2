d8139de6ff9d138d9d98d7042908901fb25d577e
= <anggagiantoro48@gmail.com>
2019-02-10 00:15:13 +0700

    tugas java

---
+++ Tugas2/src/com/base/Rectangle.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.base;
+
+/**
+ *
+ * @author admin
+ */
+public class Rectangle extends Shape {
+
+    public Rectangle(String name, String area) {
+        super(name, area);
+    }
+
+    @Override
+    public String getArea() {
+        return super.getArea();
+    }
+
+    @Override
+    public String getName() {
+        return super.getName();
+    }
+    
+    
+    
+}
