229abace34ad9db8716c1617c52281af6b265516
= <anggagiantoro48@gmail.com>
2019-02-10 10:46:33 +0700

    Tugas Java

---
+++ Tugas2/Circle.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.base;
+
+/**
+ *
+ * @author admin
+ */
+public class Circle extends Shape {
+
+    public Circle(String name, String area) {
+        super(name, area);
+    }
+
+    @Override
+    public String getArea() {
+        return super.getArea() + "( Area Circle )"; 
+    }
+
+    @Override
+    public String getName() {
+        return super.getName() + "( Circle )"; 
+    }
+    
+        
+}
